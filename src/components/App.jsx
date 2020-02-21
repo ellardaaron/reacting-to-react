d261340eeb7d3c587013e4253da687c55839056f
Aaron Ellard <student@InnovateBhams-MacBook-Pro.local>
2020-02-21 15:16:01 -0600

    finished

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,26 +3,55 @@ import React, { Component } from 'react'
 export class App extends Component {
     state = {
         text: "The FitnessGram Pacer Test",
-        newProperty:""
+        newProperty: "",
+        hasLoaded: false
     }
 
-onChange = (e) => this.setState({newProperty: e.target.value});
+    typingInBox = (e) => this.setState({ newProperty: e.target.value });
+
+    toggleFalseTrue = () => {
+        this.setState({
+            hasLoaded: !this.state.hasLoaded
+        })
+    }
+    onSubmit = (e) => {
+        e.preventDefault();
+    }
 
     render() {
-        return (
-            <div>
-                <h1>
-                    {this.props.hello}
-                    <br />
-                    {this.state.text}
-                </h1>
-                <input 
-                placeholder= {this.state}
-                value={this.state.newProperty}
-                onChange={this.onChange}
-                ></input>
-            </div>
-        )
+        if (this.state.hasLoaded === false) {
+            return (
+                <>
+                    <h1>Loading...</h1>
+                    <button
+                        onSubmit={this.onSubmit}
+                        onClick={this.toggleFalseTrue}
+                    >Submit</button>
+                </>
+            )
+        } else {
+            return (
+                <>
+                    <h1>
+                        {this.props.hello}
+                        <br />
+                        {this.state.text}
+                    </h1>
+
+                    <input
+                        placeholder="Type here..."
+                        value={this.state.newProperty}
+                        onChange={this.typingInBox}
+                    ></input>
+                    <button
+                        onSubmit={this.onSubmit}
+                        onClick={this.toggleFalseTrue}
+                    >Submit</button>
+
+                </>
+            )
+        }
+
     }
 }
 
