d73343974e7d38461f2880845b7b28d378ff812f
ellardaaron <59664272+ellardaaron@users.noreply.github.com>
2020-02-20 23:00:10 -0600

    working on it

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,12 @@ import React, { Component } from 'react'
 
 export class App extends Component {
     state = {
-        text: "The FitnessGram Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly but gets faster each minute after you hear this signal bodeboop. A single lap should be completed every time you hear this sound. ding Remember to run in a straight line and run as long as possible. The second time you fail to complete a lap before the sound, your test is over. The test will begin on the word start. On your mark. Get ready!… Start. ding"
+        text: "The FitnessGram Pacer Test",
+        newProperty:""
     }
-    
+
+onChange = (e) => this.setState({newProperty: e.target.value});
+
     render() {
         return (
             <div>
@@ -12,7 +15,12 @@ export class App extends Component {
                     {this.props.hello}
                     <br />
                     {this.state.text}
-                    </h1>
+                </h1>
+                <input 
+                placeholder= {this.state}
+                value={this.state.newProperty}
+                onChange={this.onChange}
+                ></input>
             </div>
         )
     }
